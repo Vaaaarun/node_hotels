a8eafcc1e848a89a9425ea41d2de187f13e06b30
Varun Ramgarhia <varun010204@gmail.com>
2024-07-06 17:58:46 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
 const db =require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
+const PORT = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   res.send('Welcome to our Hotel')  
@@ -20,7 +21,7 @@ app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
   console.log('listening to port 3000');
 })
 
